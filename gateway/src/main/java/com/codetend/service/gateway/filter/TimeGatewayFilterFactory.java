f1da010d6e887adb01814ce39df16c22636b7369
GaoZhenGit <gz222@126.com>
2023-01-29 14:47:14 +0800

    gateway局部过滤器完善

--- gateway/src/main/java/com/codetend/service/gateway/filter/TimeGatewayFilterFactory.java
+++ gateway/src/main/java/com/codetend/service/gateway/filter/TimeGatewayFilterFactory.java
@@ -5,14 +5,14 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -24,35 +24,36 @@ public class TimeGatewayFilterFactory extends AbstractGatewayFilterFactory<TimeG
 
     @Override
     public List<String> shortcutFieldOrder() {
-        return Collections.singletonList("show");
+        return Arrays.stream(Config.class.getDeclaredFields()).map(Field::getName).collect(Collectors.toList());
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        return new GatewayFilter() {
-            @Override
-            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-                exchange.getAttributes().put(BEGIN_TIME, System.currentTimeMillis());
-                /**
-                 *  pre的逻辑
-                 * chain.filter().then(Mono.fromRunable(()->{
-                 *     post的逻辑
-                 * }))
-                 */
-                log.info("gateway config:" + config);
-                return chain.filter(exchange).then(Mono.fromRunnable(()->{
-                    Long startTime = exchange.getAttribute(BEGIN_TIME);
-                    if (startTime != null) {
-                        log.info("{}请求耗时：{}", exchange.getRequest().getURI(), System.currentTimeMillis() - startTime);
+        return (exchange, chain) -> {
+            exchange.getAttributes().put(BEGIN_TIME, System.currentTimeMillis());
+            /*
+             *  pre的逻辑
+             * chain.filter().then(Mono.fromRunable(()->{
+             *     post的逻辑
+             * }))
+             */
+            log.info("gateway config:" + config);
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                Long startTime = exchange.getAttribute(BEGIN_TIME);
+                if (startTime != null) {
+                    long cost = System.currentTimeMillis() - startTime;
+                    if (cost > config.threshold) {
+                        log.info("{}请求耗时：{}, 阈值:{}", exchange.getRequest().getURI(), cost, config.threshold);
                     }
-                }));
-            }
+                }
+            }));
         };
     }
     @Setter
     @Getter
     @ToString
     static class Config{
-        private String show;
+        public String show;
+        public long threshold;
     }
 }
