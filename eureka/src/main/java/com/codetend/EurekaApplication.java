d8f4c87d970b48bcc88bf604f56734afb36b0db1
GaoZhenGit <912815282@qq.com>
2024-05-09 00:42:58 +0800

    eureka增加spring security

--- eureka/src/main/java/com/codetend/EurekaApplication.java
+++ eureka/src/main/java/com/codetend/EurekaApplication.java
@@ -3,11 +3,28 @@ package com.codetend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**").disable()
+                    .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .and()
+                    .httpBasic()
+            ;
+        }
     }
 }
