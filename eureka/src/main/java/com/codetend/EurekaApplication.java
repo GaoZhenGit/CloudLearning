c36bd65c361b0c6309046231ddc4cb66f3836bef
GaoZhenGit <912815282@qq.com>
2024-05-09 01:39:33 +0800

    eureka maven使用profiles支持选择打包war还是jar

--- eureka/src/main/java/com/codetend/EurekaApplication.java
+++ eureka/src/main/java/com/codetend/EurekaApplication.java
@@ -2,6 +2,8 @@ package com.codetend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,9 +11,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaApplication.class);
     }
 
     @EnableWebSecurity
